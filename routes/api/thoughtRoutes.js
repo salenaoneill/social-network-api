0ed1842e037e47cdd0609630ea15ba31b96ceea8
Salena O'Neill <salenaoneill@salenas-air.lan>
2023-03-14 12:20:41 -0500

    routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,32 @@
+//require express 
+const router = require('express').Router();
+
+//import thought controllers from thoughtController.js
+const {
+    getThought,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require('../../controllers.thoughtController');
+
+//defines GET all and POST api/thoughts routes
+router.route('/')
+.get(getThought)
+.post(createThought);
+
+//defines GET one thought, PUT, & DELETE by id api/thoughts routes
+router.route('/:thoughtId')
+.get(getSingleThought)
+.put(updateThought)
+.delete(deleteThought);
+
+//defines POST new reaction api/thoughts routes
+router.route('/:thoughtId/reactions')
+.post(createReaction);
+
+//defines DELETE a reaction by id api/thoughts routes
+//export router
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,31 @@
+//require express 
+const router = require('express').Router();
+//import user controllers from userController.js
+const {
+    getUser,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers.userController');
+
+//defines GET all and POST api/users routes
+router.route('/')
+.get(getUser)
+.post(createUser);
+
+//defines GET one user, PUT, & DELETE by id  api/users routes
+router.route('/:userId')
+.get(getSingleUser)
+.put(updateUser)
+.delete(deleteUser);
+
+//defines POST and DELETE a friend by id api/users routes
+router.route('/:userId/friends/:friendId')
+.post(addFriend)
+.delete(deleteFriend);
+
+//export router
+module.exports = router;
